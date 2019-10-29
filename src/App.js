e8d9553374ec8ece8416da97574a98f64f02e278
Subraiz <subraizahmed@gmail.com>
2019-10-28 20:06:33 -0400

    Added navigation placeholder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Home, About, Events, Projects, Resources, Team } from "./screens";
+import { Navigation } from "./components";
 
 class App extends Component {
   render() {
