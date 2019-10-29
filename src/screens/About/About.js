2aaf430ef2ae0b7b1c1c2e48a78acfdb96cda66c
Subraiz <subraizahmed@gmail.com>
2019-10-28 20:04:13 -0400

    Set up screens

--- src/screens/About/About.js
+++ src/screens/About/About.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #e7e7e7;
+`;
+
 class About extends Component {
   render() {
-    return <div />;
+    return (
+      <Container>
+        <p>About</p>
+      </Container>
+    );
   }
 }
 
--- src/screens/Events/Events.js
+++ src/screens/Events/Events.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 class Events extends Component {
   render() {
-    return <div />;
+    return (
+      <Container>
+        <p>Events</p>
+      </Container>
+    );
   }
 }
 
