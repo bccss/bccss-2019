5cef7ce1ad6bfd34173696e56e10002ea9799821
Subraiz <subraizahmed@gmail.com>
2019-11-11 20:08:52 -0500

    home page design stuff

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,77 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import LogoSrc from "../../assets/images/Logo.svg";
+
+const NavigationContainer = styled.div`
+  position: absolute;
+  display: flex;
+  width: 100vw;
+  padding: 15px 15px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled.img`
+  left: 15px;
+  position: absolute;
+`;
+
+const NavItemsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-self: center;
+`;
+
+let NavItem = styled.p`
+  padding: 0px 15px;
+  font-family: "Fira Sans Condensed", sans-serif;
+  font-weight: 200;
+  font-size: 20px;
+`;
+
+let SelectedNavItem = styled.p`
+  padding: 0px 15px;
+  font-family: "Fira Sans Condensed", sans-serif;
+  font-weight: 500;
+  color: #35298f;
+  font-size: 20px;
+`;
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      screens: [
+        { name: "Home", screen: "Home" },
+        { name: "About Us", screen: "About" },
+        { name: "Projects", screen: "Projects" },
+        { name: "Resources", screen: "Resources" },
+        { name: "Team", screen: "Team" }
+      ],
+      activeScreen: "Home"
+    };
+  }
+
+  renderNavItems = () => {
+    const { screens, activeScreen } = this.state;
+
+    return screens.map((screen, i) => {
+      if (screen.name === activeScreen) {
+        return <SelectedNavItem key={i}>{screen.name}</SelectedNavItem>;
+      } else {
+        return <NavItem key={i}>{screen.name}</NavItem>;
+      }
+    });
+  };
+
   render() {
-    return <div />;
+    return (
+      <NavigationContainer>
+        <Logo src={LogoSrc} />
+        <NavItemsContainer>{this.renderNavItems()}</NavItemsContainer>
+      </NavigationContainer>
+    );
   }
 }
 
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,28 +1,35 @@
 import React, { Component } from "react";
 import Typing from "react-typing-animation";
 import styled from "styled-components";
-import { EventCard } from "../../components";
-
-import FlyingBirds from "./FlyingBirds";
-import Background from "./Background";
+import Wave1Src from "../../assets/images/Wave1.svg";
+import Wave2Src from "../../assets/images/Wave2.svg";
+import GassonSrc from "../../assets/images/Gasson.svg";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: white;
   position: relative;
+  margin: 0;
+  padding: 0;
 `;
 
 const HeaderText = styled.p`
   text-align: center;
-  font-size: 72px;
+  font-size: 82px;
+  margin: 0;
+  padding: 0;
+  font-family: "Fira Sans Condensed", sans-serif;
+  font-weight: 500;
+  padding-top: 150px;
   /* margin: 50px; */
 `;
 
 const SubtitleText = styled.p`
-  font-family: "Press Start 2P", cursive;
   text-align: center;
-  font-size: 42px;
+  font-size: 54px;
+  font-family: "Fira Sans Condensed", sans-serif;
+  font-weight: 300;
+  margin: 0;
   /* margin: 25px 25px 50px; */
 `;
 
@@ -36,15 +43,20 @@ const InputContainer = styled.div`
   width: 100%;
 `;
 
+const Gasson = styled.img`
+  margin-bottom: -20px;
+`;
+
 const EmailInput = styled.input`
-  padding: 10px 20px;
+  padding: 10px 35px;
   border-radius: 35px;
-  border: 1px solid black;
-  font-size: 22px;
+  border: 4px solid #35298f;
+  background-color: white;
+  font-size: 20px;
 `;
 
 const Button = styled.button`
-  background-color: #ffdd57;
+  background-color: #28dc8f;
   padding: 8px 45px;
   color: white;
   border: 0px;
@@ -57,18 +69,32 @@ const Button = styled.button`
   }
 `;
 
-const EventCardContainer = styled.div`
+const WaveContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
   display: flex;
-  justify-content: center;
-  margin-top: 40px;
+`;
+
+const Wave1 = styled.img`
+  position: absolute;
+  right: 0;
+`;
+
+const Wave2 = styled.img`
+  position: absolute;
+  left: 0;
+  bottom: -150px;
 `;
 
 class Home extends Component {
   render() {
     return (
       <Container>
-        {/* <FlyingBirds /> */}
-        {/* <Background /> */}
+        <WaveContainer>
+          <Wave1 src={Wave1Src} />
+          <Wave2 src={Wave2Src} />
+        </WaveContainer>
         <HeaderText>Boston College</HeaderText>
         <Typing speed={100}>
           <SubtitleText>{"       "}</SubtitleText>
@@ -76,10 +102,8 @@ class Home extends Component {
           <SubtitleText>Computer Science Society</SubtitleText>
           <Typing.Delay ms={100000} />
         </Typing>
-        <EventCardContainer>
-          <EventCard />
-        </EventCardContainer>
         <InputContainer>
+          <Gasson src={GassonSrc} />
           <EmailInput placeholder="student@bc.edu" />
           <Button>Join</Button>
         </InputContainer>
