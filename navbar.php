6f14fd147bbaf49988f0bc0986bf2bb58b4d860f
Nathaniel Rankin <18nrankin@gmail.com>
2020-10-05 12:15:02 -0400

    Completed transition. Just need to add navbar functionality

--- navbar.php
+++ navbar.php
@@ -8,14 +8,14 @@
 </head>
 
 <div class="navigation-container">
-    <img src="src/img/Logo.svg" class="logo">
+    <img src="img/Logo.svg" class="logo">
     <div class="navitems-container">
         <div class="navitem" selected="true">Home</div>
         <div class="navitem" selected="false">About Us</div>
         <div class="navitem" selected="false">Team</div>
         <div class="navitem" selected="false">Events</div>
-        <div class="navitem" selected="false">Wiki</div>
-        <div class="navitem" selected="false">Blog</div>
+        <!-- <div class="navitem" selected="false">Wiki</div>
+        <div class="navitem" selected="false">Blog</div> -->
     </div>
 </div>
 
