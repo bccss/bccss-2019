e9baae425fa8f8e2f1f5b02f86faf5b369af7c7d
Nathaniel Rankin <18nrankin@gmail.com>
2020-10-05 15:37:42 -0400

    Fixed navbar

---
+++ js/type.js
@@ -0,0 +1,5 @@
+var subTitle = document.getElementById('sub-title');
+var typewriter = new Typewriter(subTitle, {});
+typewriter.typeString('Computer Science Society')
+    .pauseFor(2500)
+    .start();
